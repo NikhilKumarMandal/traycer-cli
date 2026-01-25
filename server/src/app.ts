3bb1bb030896a66c9e6b383e6686ad8139b058f0
Nikhil <kumarnikhil48578@gmail.com>
2026-01-25 14:31:25 +0530

    feat: connect db and setup better-auth

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,22 @@
 import express, { Request, Response } from "express";
-import { globalErrorHandler } from "./common/middleware/globalErrorHandler";
+// import { globalErrorHandler } from "./common/middleware/globalErrorHandler";
 import cookieParser from "cookie-parser";
-
+import { fromNodeHeaders, toNodeHandler } from "better-auth/node";
+import cors from "cors"
+import { auth } from "./lib/auth";
 const app = express();
+const port =  process.env.PORT ?? 4000
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
 app.use(cookieParser());
 app.use(express.json());
 
@@ -10,6 +24,12 @@ app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello from order service service!" });
 });
 
-app.use(globalErrorHandler);
+// app.use(globalErrorHandler);
+
+app.listen(port, () => {
+  console.log(`Traycer app listening on port ${port}`);
+});
+
+
 
 export default app;
